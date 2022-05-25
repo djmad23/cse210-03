a499d5934bc692b157805116871aa1373983a933
ryanscom <ryanscom@byui.edu>
2022-05-25 16:34:29 -0700

    updated file path for word.txt

--- jumper/game/word.py
+++ jumper/game/word.py
@@ -1,5 +1,5 @@
 
-
+import os
 import random
 # from game.words_list import word_list
 
@@ -21,11 +21,8 @@ class Word:
             secret_word (str): The secret word (randomly generated from a text file.)
         """
         
-        # self._word_normal = random.choice(word_list)
-        # self._word = self._word_normal.upper()
-
         self._word = self.get_secret_word()
-        # self._word = self._word_normal.upper()
+        
 
         self._hidden_word = ("_") * len(self._word)
         self._hidden_word_as_list = list(self._hidden_word)
@@ -38,38 +35,54 @@ class Word:
         self._first_guess = False
 
     def get_secret_word(self):
-        with open("words.txt","r") as word:
+        """ Gets a random word from a text file and makes it uppercase.
+        """
+        with open(os.path.abspath("cse210-03/jumper/game/words.txt"),"r") as word:
             lines = word.readlines()
             secret_word = random.choice(lines)
             secret_word = secret_word.strip()
         return secret_word.upper()
 
+
     def get_first_guess(self):
-         
-         return self._first_guess
+        """Returns first guess boolean
+        """ 
+        return self._first_guess
         
     def get_hidden_word(self): # Method for testing if hidden word is working correctly. Coder can print to screen to 
-
+        """Returns hidden word as list 
+        """
         return self._hidden_word_as_list
 
     def get_word(self): # Method to get the word. (NOT HIDDEN)
-
+        """Returns the chosen random word. (NOT HIDDEN)
+        """
         return self._word
 
     def get_guessed_bool(self): # Method to check if the last guess was in the word of not. True/False.
-
+        """Returns the boolean that shows if the users guessed letter was in the word.  
+        """
         return self._guessed
 
     def get_won_bool(self): # Method to check if the hidden word has been completly guessed. True/False.
-
+        """ Returns the boolean that tells if user won the game or not.
+        """
         return self._you_won
 
     def get_missed_letters(self): # Method to get missed letter list
-
+        """Returns the missed guess letters
+        """
         return self._missed_letters
 
     def update_hidden_word(self, guess): # Method for updating the hidden word with a user guess
+        """ Updates the hidden word with correct letter guesses. Turns of the intro message by turning the first guess booleon to False.
+        Updates guessed boolean to True if guess letter is in the word, to False if it is not. 
+        Updates missed letters list if guess in not in the hidden word.
+        If hidden word is completly guessed turns you_won booleon to True
 
+        Args:
+        
+        """
         self._first_guess = True # boolean to turn off first message
 
         letter_locations = [i for i, letter in enumerate(self._word) if letter == guess]
